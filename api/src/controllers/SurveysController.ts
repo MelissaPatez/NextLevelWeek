d7bad193fe9f945a5304bf15ab0f3a3234cb36da
melissapatez <m.patez@teltecsolutions.com.br>
2021-02-24 13:36:18 -0300

    Controller e models criado para surveys

---
+++ api/src/controllers/SurveysController.ts
@@ -0,0 +1,24 @@
+import { request, Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { Suvery } from "../models/Survey";
+import { SurveysRepository } from "../repositories/SurveysRepository";
+
+class SurveysController {
+    async create(request:Request, response:Response) {
+        const { title, description } = request.body;
+
+        const surveysRepository = getCustomRepository(SurveysRepository);
+
+        const survey = surveysRepository.create({
+            title,
+            description
+        });
+
+        await surveysRepository.save(survey);
+
+        return response.status(201).json(survey);
+    }
+    
+}
+
+export { SurveysController };
