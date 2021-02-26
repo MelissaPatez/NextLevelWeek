a82bd2b3a71e0642fb93bf2af37dcdf27552eeb3
melissapatez <m.patez@teltecsolutions.com.br>
2021-02-26 16:26:37 -0300

    Criando Validacao do nome e email

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UserRepository } from "../repositories/UsersRepository";
+import * as yup from "yup";
 
 class UserController {
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
+
+        const schema = yup.object().shape({  //validação"
+            name: yup.string().required("Nome é obrigatório"),
+            email: yup.string().email().required("Email é obrigatório")
+        })
+        
+        try {
+            await schema.validate(request.body, { abortEarly: false});
+        } catch (err) {
+            return response.status(400).json({error:  err});
+        }
       
-        const userRepository = getCustomRepository(UserRepository)
+        const userRepository = getCustomRepository(UserRepository);
 
         //SELECT * FROM USERS WHERE EMAIL = "EMAIL"
         const userAlreadyExists = await userRepository.findOne({
