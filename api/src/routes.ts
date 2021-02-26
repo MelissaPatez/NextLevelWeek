76667f9fd79bac00f2ef9dbb01b3c3eaefd69ab7
melissapatez <m.patez@teltecsolutions.com.br>
2021-02-26 08:08:56 -0300

    Criando rota para envio de email

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { SendMailController } from './controllers/SandMailController';
 import { SurveysController } from "./controllers/SurveysController";
 import { UserController } from './controllers/UserController';
 const router = Router();
@@ -6,8 +7,13 @@ const router = Router();
 const userController = new UserController();
 const surveysController = new SurveysController();
 
-router.post("/users", userController.create);
+const sendMailController = new SendMailController();
+
+router.post("/users", userController.create); 
+
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
+
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
