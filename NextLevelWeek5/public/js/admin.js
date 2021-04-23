d5302a05d4f518c3f6b6c1bf3e30703db634ec71
melissapatez <m.patez@teltecsolutions.com.br>
2021-04-23 18:49:36 -0300

    Criando janela de chat para o admin

--- NextLevelWeek5/public/js/admin.js
+++ NextLevelWeek5/public/js/admin.js
@@ -13,6 +13,19 @@ socket.on("admin_list_all_users", (connections) => {
             id: connection.socket_id,
         })
         document.getElementById("list_users").innerHTML += rendered;
-    })
+    });
 
-})
+});
+
+function call(id){
+    const connection = connectionsUsers.find(
+        (connection) => connection.socket_id === id
+    );
+    const template = document.getElementById("admin_template").innerHTML;
+
+    const rendered = Mustache.render(template, {
+        email: connection.user.email,
+        id: connection.user.id
+    });
+    document.getElementById("supports").innerHTML += rendered; 
+}
